2d07195ab64cce58bfd0470b67ffef74a079eec8
thanishka778 <73109430+thanishka778@users.noreply.github.com>
2021-01-25 16:12:10 +0530

    Add files via upload

--- chat_room.js
+++ chat_room.js
@@ -12,11 +12,35 @@
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+  
+  user_name=localStorage.getItem("username");
+document.getElementById("user_name").innerHTML="Welcome " + user_name;
 
   function add_room(){
     room_name=document.getElementById("room_name").value;
     firebase.database().ref("/").child(room_name).update({
         purpose:"adding room"
     });
+    localStorage.setItem("room name", room_name);
+    window.location="kwitter_page.html";
+}
+
+
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey =
+childSnapshot.key;
+ Room_names = childKey;
+ //Start code
+ console.log("room name-"+Room_names);
+      row="<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>"
+      document.getElementById("output").innerHTML+=row;
+ //End code
+ });});}
+getData();
+
+function redirectToRoomName(name){
+  console.log(name);
+  localStorage.setItem("roomname", name);
+  window.location="chat_page.html";
 }
